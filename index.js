50f36f13bcd7eb3a08c1ce97bff649fd0d81e508
Aashray <aashray@uw.edu>
2018-11-25 22:41:35 -0800

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'})
+})
+
+// dynamically set port from environment variables (for Heroku)
+// provide alternate of port 5000 if working in dev environment
+const PORT = process.env.port || 5000
+app.listen(PORT)
